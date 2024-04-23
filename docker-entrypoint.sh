94df9ac8a6daaf88a8753567bf824a6f4517831f
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2024-04-23 15:11:07 +0200

    add pyos nslookup test loop

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-export PYOS_FQDN=$(nslookup pyos | awk '/^Name:/ { print $2 }')
-echo PYOS_FQDN=$PYOS_FQDN
+PYOS_SERVER_NAME=pyos
+
+while true; do
+        if nslookup $PYOS_SERVER_NAME
+        then
+          break
+        else
+          echo "nslookup $PYOS_SERVER_NAME command as failed, sleeping for 1s"
+          sleep 1s
+        fi
+done
+
+PYOS_FQDN=$(nslookup $PYOS_SERVER_NAME | awk '/^Name:/ { print $2 }')
+echo "PYOS_FQDN=$PYOS_FQDN"
 
 nginx -g 'daemon off;'
