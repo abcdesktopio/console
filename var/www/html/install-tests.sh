8b57047fa8efafd99f6be67a629b605145227ec3
Matt307082 <matteobeghelli2003@gmail.com>
2024-07-08 09:38:26 +0200

    update install-tests.sh

--- var/www/html/install-tests.sh
+++ var/www/html/install-tests.sh
@@ -3,7 +3,7 @@
 echo "apt-get update"
 apt-get update
 echo "apt-get install -y curl gpg"
-apt-get install -y --no-install-recommends crul gnupg
+apt-get install -y --no-install-recommends curl gnupg ca-certificates
 
 
 echo "install yarn npm nodejs "
@@ -11,7 +11,7 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg 
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list 
 apt-get update
-apt-get install -y --no-install-recommends nodejs 
+apt-get install -y --no-install-recommends nodejs npm
 npm -g install yarn  
 
 echo "install tests packages for console"
