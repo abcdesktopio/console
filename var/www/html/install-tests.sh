89afb24758ce46a672adceba8f8485b64e34ba5f
Matt307082 <matteobeghelli2003@gmail.com>
2024-07-08 09:54:17 +0200

    remove apt-get install npm

--- var/www/html/install-tests.sh
+++ var/www/html/install-tests.sh
@@ -11,9 +11,11 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg 
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list 
 apt-get update
-apt-get install -y --no-install-recommends nodejs npm
+apt-get install -y --no-install-recommends nodejs
 npm -g install yarn  
 
 echo "install tests packages for console"
 cd /var/www/html
-yarn install --productuon=false && npm i --package-lock-only && npm audit fix
+yarn install --productuon=false 
+npm i --package-lock-only 
+npm audit fix
