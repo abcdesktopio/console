bda203005a0c22f152cb05970a9a434df3bfc31f
Matt307082 <matteobeghelli2003@gmail.com>
2024-07-10 14:04:54 +0200

    add user to docker group

--- var/www/html/install-tests.sh
+++ var/www/html/install-tests.sh
@@ -27,3 +27,5 @@ echo "install docker"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-get install -y --no-install-recommends docker-ce
+groupadd docker && usermod -aG docker $USER && newgrp docker
+docker run hello-world
