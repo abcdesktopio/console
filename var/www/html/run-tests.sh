4c06548b76b54cc46d6008970652cb2c5baa8875
Matt307082 <matteobeghelli2003@gmail.com>
2024-07-12 11:38:43 +0200

    retrieve abcdesktop url form parsing output

--- var/www/html/run-tests.sh
+++ var/www/html/run-tests.sh
@@ -9,9 +9,18 @@ curl --progress-bar "$ABCDESKTOP_YAML_SOURCE" --output abcdesktop.yaml
 sed -i'' -e "s|image: abcdesktopio/console:3.3|image: abcdesktopio/console:test.$1|g" abcdesktop.yaml
 
 #install deploy abcdesktop locally on the container
-curl -sL https://raw.githubusercontent.com/abcdesktopio/conf/main/kubernetes/install-3.3.sh | bash
+output =$(curl -sL https://raw.githubusercontent.com/abcdesktopio/conf/main/kubernetes/install-3.3.sh | bash)
+
+# Extract the abcdesktop URL
+url=$(echo "$output" | grep -oP 'http://[0-9.]+:[0-9]+/' | tail -n 1)
+
+# Check if the URL was successfully extracted
+if [ -z "$url" ]; then
+    echo "Failed to retrieve the abcdesktop URL"
+    exit 1
+fi
 
 cd var/www/html
 
 #run the acutal test
-npm run test -- url=http://$MY_IP:$port/
+npm run test -- url=$url
