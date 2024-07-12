f78e20ebfd3aad96c6acc0e3f46197d706d1975f
Matt307082 <matteobeghelli2003@gmail.com>
2024-07-12 12:04:29 +0200

    add temp file to store abcdesktop install output

--- var/www/html/run-tests.sh
+++ var/www/html/run-tests.sh
@@ -8,11 +8,24 @@ curl --progress-bar "$ABCDESKTOP_YAML_SOURCE" --output abcdesktop.yaml
 #replacing console base image by the test image
 sed -i'' -e "s|image: abcdesktopio/console:3.3|image: abcdesktopio/console:test.$1|g" abcdesktop.yaml
 
+#create a temporary file to store the output
+temp_file=$(mktemp)
+
 #install deploy abcdesktop locally on the container
-output =$(curl -sL https://raw.githubusercontent.com/abcdesktopio/conf/main/kubernetes/install-3.3.sh | bash | sudo tee /dev/tty)
+curl -sL https://raw.githubusercontent.com/abcdesktopio/conf/main/kubernetes/install-3.3.sh | bash > "$temp_file" 2>&1
+
+if [ $? -ne 0 ]; then
+    echo "abcdesktop install script failed to execute."
+    exit 1
+fi
+
+cat "$temp_file"
 
 #extract the abcdesktop URL
-url=$(echo "$output" | grep -oP 'http://[0-9.]+:[0-9]+/' | tail -n 1)
+url=$(grep -oP 'http://[0-9.]+:[0-9]+/' "$temp_file" | tail -n 1)
+
+#clean up the temporary file
+rm "$temp_file"
 
 #check if the URL was successfully extracted
 if [ -z "$url" ]; then
