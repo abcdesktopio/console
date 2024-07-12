a82ff34612dbad8c42fbc77ce43b90d54b5fa1b7
Matt307082 <matteobeghelli2003@gmail.com>
2024-07-12 10:09:06 +0200

    fixed missing sudo

--- var/www/html/install-tests.sh
+++ var/www/html/install-tests.sh
@@ -24,7 +24,7 @@ sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable mai
 wget -O- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/linux_signing_key.pub 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 78BD65473CB3BD13 
 sudo apt-get update 
-sudo apt-key export D38B4796 | gpg --dearmour -o /etc/apt/trusted.gpg.d/chrome.gpg 
+sudo apt-key export D38B4796 | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/chrome.gpg 
 sudo apt-get install -y --no-install-recommends google-chrome-stable 
 
 # echo "install kubectl"
