8753ee6edabdabbf8469f3ccef461e47603c17b9
Matt307082 <matteobeghelli2003@gmail.com>
2024-07-12 10:04:59 +0200

    fixed sed command

--- var/www/html/run-tests.sh
+++ var/www/html/run-tests.sh
@@ -6,7 +6,7 @@ ABCDESKTOP_YAML_SOURCE="https://raw.githubusercontent.com/abcdesktopio/conf/main
 curl --progress-bar "$ABCDESKTOP_YAML_SOURCE" --output abcdesktop.yaml
 
 #replacing console base image by the test image
-sed -i "571s/^[[:space:]]*/&image: abcdesktopio/console:test.$1/" abcdesktop.yaml
+sed -i'' -e "s|image: abcdesktopio/console:3.3|namespace: abcdesktopio/console:test.$1|g" abcdesktop.yaml
 
 #install deploy abcdesktop locally on the container
 curl -sL https://raw.githubusercontent.com/abcdesktopio/conf/main/kubernetes/install-3.3.sh | bash
