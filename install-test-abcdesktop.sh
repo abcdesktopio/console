c3d16630213344dc65e4edbaaa8a8ad4745f3184
Matt307082 <matteobeghelli2003@gmail.com>
2025-08-19 15:50:19 +0200

    uses cypress github action

---
+++ install-test-abcdesktop.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+VERSION=4.1
+ABCDESKTOP_YAML_SOURCE="https://raw.githubusercontent.com/abcdesktopio/conf/main/kubernetes/abcdesktop-$VERSION.yaml"
+
+#downloading abcdesktop.yaml file
+curl --progress-bar "$ABCDESKTOP_YAML_SOURCE" --output abcdesktop.yaml
+
+#replacing console base image by the test image
+sed -i'' -e "s|image: abcdesktopio/console:3.3|image: ghcr.io/abcdesktopio/console:test.$1|g" abcdesktop.yaml
+
+#create a temporary file to store the output
+temp_file=$(mktemp)
+
+echo "installing abcdesktop"
+
+#install deploy abcdesktop locally on the container
+curl -sL https://raw.githubusercontent.com/abcdesktopio/conf/main/kubernetes/install-$VERSION.sh | bash > "$temp_file" 2>&1
+
+#print abcdesktop install output
+cat "$temp_file"
+
+if [ $? -ne 0 ]; then
+    echo "abcdesktop install script failed to execute."
+    exit 1
+fi
+
+#extract the abcdesktop URL
+url=$(grep -oP 'http://[0-9.]+:[0-9]+/' "$temp_file" | tail -n 1)
+
+#clean up the temporary file
+rm "$temp_file"
+
+#check if the URL was successfully extracted
+if [ -z "$url" ]; then
+    echo "Failed to retrieve the abcdesktop URL"
+    exit 1
+fi
+
+echo "abcdesktop url is $url"
---
+++ install-tests-dependencies.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "apt-get update"
+sudo apt-get update
+echo "apt-get install -y apt-transport-https ca-certificates curl software-properties-common gpg openssl sed"
+sudo apt-get install -y --no-install-recommends curl gnupg ca-certificates openssl apt-transport-https software-properties-common sed
+
+echo "install yarn npm nodejs "
+mkdir -p /etc/apt/keyrings 
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg 
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list 
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends nodejs
+sudo npm -g install yarn  
+
+echo "install tests packages for console"
+cd app/
+sudo yarn install --production=false 
+sudo npm i --package-lock-only 
+sudo npm audit fix
